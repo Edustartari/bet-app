a0ce3e47f975dbed779cc2faf366ab640ae52db4
Edustartari <edustartari@gmail.com>
2022-02-16 20:28:34 -0300

    add models and makemigration

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.CharField(max_length=100)
+    image = models.CharField(max_length=200, default='')
+    hash_id = models.CharField(max_length=200, default=None)
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class bet(models.Model):
+    hash_id = models.CharField(max_length=200, default=None)
+    pool_id = models.IntegerField(default=0)
+    user_id = models.IntegerField(default=0)
+    question_id = models.IntegerField(default=0)
+    bet_data = models.TextField(default='')
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+class pool(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.CharField(max_length=200, default=None)
+    hash_id = models.CharField(max_length=200, default=None)
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    finish_date = models.DateField()
+    is_active = models.IntegerField(default=0)
+    is_private = models.IntegerField(default=0)
+    theme = models.CharField(max_length=200)
+
+class questions(models.Model):
+    question_title = models.CharField(max_length=150)
+    question_description = models.CharField(max_length=500, default='')
+    question_data = models.TextField(default='')
+    image = models.CharField(max_length=200, default=None)
+    hash_id = models.CharField(max_length=200, default=None)
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    question_type = models.CharField(max_length=200)
+    pool_id = models.IntegerField(default=0)
+    answer = models.CharField(max_length=200)
+    is_active = models.IntegerField(default=0)
+
+class pool_admins(models.Model):
+    pool_id = models.IntegerField(default=0)
+    user_id = models.IntegerField(default=0)
+    hash_id = models.CharField(max_length=200, default=None)
+    is_admin = models.IntegerField(default=0)
+    created_at = models.DateField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
