6fec17da9a77033249c30fad047b248b67b46507
Edustartari <edustartari@gmail.com>
2025-06-11 22:03:37 -0300

    change database HOST

--- betApp/settings.py
+++ betApp/settings.py
@@ -110,7 +110,8 @@ DATABASES = {
         'NAME': 'bet_app_database',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
+        # Accept localhost and other values for 'HOST'
+        'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
